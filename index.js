386840f9792f3da3176a9e2d37db6a7dfa5fce06
luzilong <luzilong@mininglamp.com>
2021-12-27 18:31:53 +0800

    express基本搭建

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/test', (req, res, next) => {
+    console.log('test1');
+    next()
+});
+
+app.get('/test', (req, res, next) => {
+    console.log('test2');
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+    res.end('我是test页面');
+})
+
+
+app.listen(8888, () => {
+    console.log('服务启动');
+})
